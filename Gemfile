c36ee9d40f10b22b7a1d2bfea42d8f4ac7dbf8d3
Alejandro <karnal02@gmail.com>
2013-12-16 17:03:35 -0600

    primer commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'unicorn'
+gem 'haml'
+gem 'haml-rails'
+gem 'aws-sdk'
+gem 'nested_set'
+gem 'devise'
+gem 'paperclip'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'newrelic_rpm'
+gem 'instagram'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development do
+  gem 'guard'
+  gem 'guard-livereload'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MK1::Application.config.secret_key_base = '3102897a53fd5ebb0256f38fd58bd25b8da42f540ea39399cd3da75bfba44a4b3b4dbd58bccb4aa3da6b265511f90b8e680d69c1f718d7004255fca1c03b2c3c'
