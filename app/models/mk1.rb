0d3c519e87c25f783aa484f9b3ce9a8e143374e9
Alejandro <karnal02@gmail.com>
2013-12-16 17:24:24 -0600

    admin

---
+++ app/models/mk1.rb
@@ -0,0 +1,2 @@
+class Mk1 < ActiveRecord::Base
+end
---
+++ db/migrate/20131216232018_create_mk1s.rb
@@ -0,0 +1,16 @@
+class CreateMk1s < ActiveRecord::Migration
+  def change
+    create_table :mk1s do |t|
+      t.string :instagram_id
+      t.string :instagram_link
+      t.string :pic_thumb
+      t.string :pic_med
+      t.string :pic_large
+      t.string :fullname
+      t.string :username
+      t.boolean :publicado, :default => false
+
+      t.timestamps
+    end
+  end
+end
