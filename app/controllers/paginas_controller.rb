e3c4213f2dcaa064c1fbc7cf2582c08a82a37deb
Alejandro <karnal02@gmail.com>
2013-12-16 17:46:58 -0600

    prueba instragram

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -1,4 +1,30 @@
 class PaginasController < ApplicationController
   def home
+    @fotos = Sephoragram.order('created_at DESC').all
+  end
+
+  def new_stuff
+    Instagram.process_subscription(request.body.read) do |handler|
+      handler.on_tag_changed do |tag, data|
+        @inst = Instagram.tag_recent_media("mk1");
+        @inst.each do |data|
+          sephoragram = Sephoragram.find_or_create_by_instagram_id(
+            :instagram_id => data.id,
+            :instagram_link => data.link,
+            :pic_thumb => data.images.thumbnail.url,
+            :pic_med => data.images.low_resolution.url,
+            :pic_large => data.images.standard_resolution.url,
+            :fullname => data.user.full_name,
+            :username => data.user.username
+          )
+          sephoragram.save
+        end
+      end
+    end
+    render :text => ""
+  end
+
+  def suscribir
+    render :text => "#{params['hub.challenge']}"
   end
 end
