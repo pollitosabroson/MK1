3b9172738b394374adc41e20135e775bd3a02f16
Alejandro <karnal02@gmail.com>
2013-12-20 15:54:25 -0600

    callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter do
+    CALLBACK_URL = "http://mk1.herokuapp.com"
+
+    Instagram.configure do |config|
+      config.client_id = ENV['INSTA_APP']
+      config.client_secret = ENV['INSTA_SEC']
+    end
+  end
 end
