398fdcecf9a438a5cba66ddc2f61bef7d6c95a97
pollitosabroson <karnal02@gmail.com>
2013-12-25 14:07:50 -0600

    login prueba 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter do
-    CALLBACK_URL = "http://mk1.herokuapp.com"
-
-    Instagram.configure do |config|
-      config.client_id = ENV['INSTA_APP']
-      config.client_secret = ENV['INSTA_SEC']
-    end
-  end
+  
 end
